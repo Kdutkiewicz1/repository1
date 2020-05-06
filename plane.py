140cfbda647882b96cf895f5b4f9d86bee90edc6
Kdutkiewicz1 <61584646+Kdutkiewicz1@users.noreply.github.com>
2020-05-06 20:50:32 +0200

    Update plane.py

--- plane.py
+++ plane.py
@@ -1,6 +1,6 @@
 import logging
 import multiprocessing as mp
-from plane1 import Plane
+from jhhjhjssdsd import Plane
 from abc import ABC, abstractmethod
 import random
 
@@ -24,27 +24,26 @@ class Turbulance(Event):
 class Correction(Event):
 
     def change_degree(self):
-        value = random.gauss(0, 2 * random.randint(1, 10))
+        value = random.gauss(0, 2 * random.randint(1, 9))
         self.degree = value
         return self.degree
 
 
 def main():
-    plane = Plane()
-    turbulence = Turbulance()
-    corr = Correction()
-    logging.basicConfig(filename='info.log', filemode='w', level=logging.INFO,
-                        format='%(asctime)s- %(levelname)s- %(message)s')
-    logging.info('plane orientation {} '.format(plane.begin))
-    plane.tilt(turbulence.change_degree())
-    logging.warning('plane orientation after turbulance {} '.format(plane.begin))
-    plane.tilt(corr.change_degree())
-    logging.warning('plane orientation after correction {}'.format(plane.begin))
+     plane = Plane()
+     turbulence = Turbulance()
+     corr = Correction()
+     logging.basicConfig(filename='info.log', filemode='w', level=logging.INFO, format ='%(asctime)s %(message)s')
+     logging.info('plane orientation {} '.format(plane.begin))
+     plane.tilt(turbulence.change_degree())
+     logging.warning('plane orientation after turbulance {} '.format(plane.begin))
+     plane.tilt(corr.change_degree())
+     logging.warning('plane orientation after correction {}'.format(plane.begin))
 
-    if __name__ == "__main__":
-        processes = [mp.Process(target=main()) for x in range(10)]
-        for p in processes:
-            p.start()
-        for p in processes:
-            p.join()
+if __name__ == "__main__":
+     processes = [mp.Process(target=main()) for x in range(10)]
+     for p in processes:
+       p.start()
+     for p in processes:
+       p.join()
 
