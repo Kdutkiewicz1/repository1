36745268653740191b8381082abfdf8cd6f155dd
Kdutkiewicz1 <61584646+Kdutkiewicz1@users.noreply.github.com>
2020-05-06 19:19:03 +0200

    Update plane.py

--- plane.py
+++ plane.py
@@ -1,6 +1,6 @@
 import logging
 import multiprocessing as mp
-from plane1 import Turbulance, Plane
+from jhhjhjssdsd import Plane
 from abc import ABC, abstractmethod
 import random
 
@@ -8,28 +8,43 @@ import random
 class Event(ABC):
 
     def __init__(self):
-        self.angle = random.randint(1, 40)
+        self.degree = random.randint(1, 90)
         super().__init__()
 
     @abstractmethod
-    def degree(self):
+    def change_degree(self):
         pass
 
+
+class Turbulance(Event):
+    def change_degree(self):
+        return self.degree
+
+
+class Correction(Event):
+
+    def change_degree(self):
+        value = random.gauss(0, 2 * random.randint(1, 9))
+        self.degree = value
+        return self.degree
+
+
 def main():
-          plane = Plane()
-          turbulence = Turbulance()
-          logging.basicConfig(filename='information.log', filemode='w', level=logging.INFO,
-          format='%(asctime)s %(message)s')
-          logging.info('plane  inizial orientation {} '.format(plane.begin))
-          plane.tilt(turbulence.correction())
-          logging.warning('plane orientation after turbolance {} '.format(plane.begin))
-          plane.tilt(turbulence.vibrations())
-          logging.warning('plane orientation after correction {}'.format(plane.begin))
+    plane = Plane()
+    turbulence = Turbulance()
+    corr = Correction()
+    logging.basicConfig(filename='info.log', filemode='w', level=logging.INFO,
+                        format='%(asctime)s- %(levelname)s- %(message)s')
+    logging.info('plane orientation {} '.format(plane.begin))
+    plane.tilt(turbulence.change_degree())
+    logging.warning('plane orientation after turbulance {} '.format(plane.begin))
+    plane.tilt(corr.change_degree())
+    logging.warning('plane orientation after correction {}'.format(plane.begin))
 
+    if __name__ == "__main__":
+        processes = [mp.Process(target=main()) for x in range(10)]
+        for p in processes:
+            p.start()
+        for p in processes:
+            p.join()
 
-          if __name__ == "__main__":
-             processes = [mp.Process(target=main()) for x in range(10)]
-             for p in processes:
-                 p.start()
-             for p in processes:
-                 p.join()
