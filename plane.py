d2540b2aac405c94652fa830483c3a73571daa25
Kdutkiewicz1 <61584646+Kdutkiewicz1@users.noreply.github.com>
2020-05-06 15:31:18 +0200

    Create plane.py

---
+++ plane.py
@@ -0,0 +1,35 @@
+import logging
+import multiprocessing as mp
+from plane1 import Turbulance, Plane
+from abc import ABC, abstractmethod
+import random
+
+
+class Event(ABC):
+
+    def __init__(self):
+        self.angle = random.randint(1, 40)
+        super().__init__()
+
+    @abstractmethod
+    def change_angle(self):
+        pass
+
+def main():
+          plane = Plane()
+          turbulence = Turbulance()
+          logging.basicConfig(filename='plane.log', filemode='w', level=logging.INFO,
+          format='%(asctime)s %(message)s')
+          logging.info('plane  inizial orientation {} '.format(plane.begin))
+          plane.tilt(turbulence.correction())
+          logging.warning('plane orientation after turbolance {} '.format(plane.begin))
+          plane.tilt(turbulence.vibrations())
+          logging.warning('plane orientation after correction {}'.format(plane.begin))
+
+
+          if __name__ == "__main__":
+             processes = [mp.Process(target=main()) for x in range(10)]
+             for p in processes:
+                 p.start()
+             for p in processes:
+                 p.join()
