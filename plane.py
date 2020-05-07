00d75d04a5022c56c9637e9df7f1cadee396b4ec
Kdutkiewicz1 <61584646+Kdutkiewicz1@users.noreply.github.com>
2020-05-07 19:25:27 +0200

    Update plane.py

--- plane.py
+++ plane.py
@@ -1,49 +1,50 @@
 import logging
-import multiprocessing as mp
-from jhhjhjssdsd import Plane
+from plane1 import Plane
 from abc import ABC, abstractmethod
 import random
+import multiprocessing as mp
 
 
 class Event(ABC):
 
     def __init__(self):
-        self.degree = random.randint(1, 90)
+        self.tilt = random.randint(1, 90)
         super().__init__()
 
     @abstractmethod
-    def change_degree(self):
+    def change_tilt(self):
         pass
 
 
 class Turbulance(Event):
-    def change_degree(self):
-        return self.degree
+    def change_tilt(self):
+        return self.tilt
 
 
 class Correction(Event):
-
-    def change_degree(self):
-        value = random.gauss(0, 2 * random.randint(1, 9))
-        self.degree = value
-        return self.degree
+      def change_tilt(self):
+          corect = random.gauss(0, 2 * random.randint(1, 10))
+          self.tilt = corect
+          return self.tilt
 
 
 def main():
-     plane = Plane()
-     turbulence = Turbulance()
-     corr = Correction()
-     logging.basicConfig(filename='info.log', filemode='w', level=logging.INFO, format ='%(asctime)s %(message)s')
-     logging.info('plane orientation {} '.format(plane.begin))
-     plane.tilt(turbulence.change_degree())
-     logging.warning('plane orientation after turbulance {} '.format(plane.begin))
-     plane.tilt(corr.change_degree())
-     logging.warning('plane orientation after correction {}'.format(plane.begin))
+    plane = Plane()
+    turbu = Turbulance()
+    corr = Correction()
+    logging.basicConfig(filename='info.log', filemode='w', level=logging.INFO,
+                        format='%(asctime)s - %(name)s - %(message)s')
+    logging.info('Corect angle:{}'.format(plane.begin))
+    plane.tilt(turbu.change_tilt())
+    logging.warning('Turbulance:{}'.format(turbu.change_tilt()))
+    plane.tilt(corr.change_tilt())
+    logging.info('Correction:{}'.format(corr.change_tilt()))
+
 
 if __name__ == "__main__":
-     processes = [mp.Process(target=main()) for x in range(10)]
-     for p in processes:
-       p.start()
-     for p in processes:
-       p.join()
+    processes = [mp.Process(target=main()) for x in range(10)]
+    for p in processes:
+        p.start()
+    for p in processes:
+        p.join()
 
